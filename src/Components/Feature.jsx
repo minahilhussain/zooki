727345b0db4fd60c086c31535b3507b90a60d96d
minahilhussain <minahilh21@gmail.com>
2022-08-29 10:09:21 +0500

    added feature content

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../css/Feature.css"
 import "../css/index.css"
+import { FaLongArrowAltRight } from "react-icons/fa"
 
 function Feature() {
   return (
@@ -9,6 +10,16 @@ function Feature() {
         <h2>our Features</h2>
         <div className='title-border-simple'>
           <div className='title-border-colored'></div>
+        </div>
+        <div className="left">
+        <h3>Marketing Performance</h3>
+        <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.
+        <br/>
+        Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
+        <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
+        </div>
+        <div className="right">
+          <img src="http://zooki.react.themesbrand.com/static/media/img-1.1bb1c555fb21d91778ad.png" alt="feature-first"/>
         </div>
       </div>
       </section>
