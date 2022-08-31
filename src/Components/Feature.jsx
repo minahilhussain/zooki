770bbf417252cb6dadf4184de290460018e29b67
minahilhussain <minahilh21@gmail.com>
2022-08-31 21:23:32 +0500

    added fesature block text and images

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import "../css/Feature.css"
 import "../css/index.css"
-import { FaLongArrowAltRight } from "react-icons/fa"
+import { FaLongArrowAltRight, FaReact } from "react-icons/fa"
 
 function Feature() {
   return (
       <section id="feature">
-      <div class="wrapper">
+      <div class="container">
         <h2>our Features</h2>
         <div className='title-border-simple'>
           <div className='title-border-colored'></div>
         </div>
-        <div className="left">
-        <h3>Marketing Performance</h3>
-        <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.
-        <br/>
-        Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
-        <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
+        <div class="icon">
+          <FaReact/>
         </div>
-        <div className="right">
-          <img src="http://zooki.react.themesbrand.com/static/media/img-1.1bb1c555fb21d91778ad.png" alt="feature-first"/>
+        <div className="wrapper">
+          <div className="left">
+            <h3>Marketing Performance</h3>
+            <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.</p>
+            <p>Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
+            <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
+          </div>
+          <div className="right">
+            <img src="http://zooki.react.themesbrand.com/static/media/img-1.1bb1c555fb21d91778ad.png" alt="feature-first"/>
+          </div>
         </div>
       </div>
       </section>
