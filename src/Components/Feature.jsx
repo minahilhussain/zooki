dd084cd05dcd030bb760087d694379d07a7dbd9a
minahilhussain <minahil21@gmail.com>
2022-09-04 20:45:31 +0500

    added feature content

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "../css/Feature.css"
 import "../css/index.css"
 import { FaLongArrowAltRight, FaReact } from "react-icons/fa"
+import { BsArrowsMove } from "react-icons/bs"
+import { AiOutlineMonitor } from "react-icons/ai"
 
 function Feature() {
   return (
@@ -11,29 +13,46 @@ function Feature() {
         <div className='title-border-simple'>
           <div className='title-border-colored'></div>
         </div>
-        <div class="icon">
-          <FaReact/>
-        </div>
         <div className="wrapper">
-          <div className="left">
+          <div className="text">
+            <span className="head-icon">
+              <FaReact/>
+            </span>
             <h3>Marketing Performance</h3>
             <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.</p>
             <p>Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
             <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
           </div>
-          <div className="right">
+          <div className="image">
             <img src="http://zooki.react.themesbrand.com/static/media/img-1.1bb1c555fb21d91778ad.png" alt="feature-first"/>
           </div>
         </div>
         <div className="wrapper">
-          <div className="left">
-            <img src="http://zooki.react.themesbrand.com/static/media/img-1.1bb1c555fb21d91778ad.png" alt="feature-first"/>
+          <div className="image">
+            <img src="http://zooki.react.themesbrand.com/static/media/img-2.73bbfc3f31770042da4d.png" alt="feature-first"/>
           </div>
-          <div className="right">
-            <h3>Marketing Performance</h3>
+          <div className="text">
+            <span className="head-icon">
+              <BsArrowsMove/>
+            </span>
+            <h3>Strategy Solutions</h3>
             <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.</p>
             <p>Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
             <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
+          </div>
+        </div>
+        <div className="wrapper">
+          <div className="text">
+            <span className="head-icon">
+              <AiOutlineMonitor/>
+            </span>
+            <h3>Marketing business</h3>
+            <p>Itaque earum rerum hic tenetur sapiente delectut reiciendis voluptatibus perspiciatis unde omnis iste natus error sit maiores alias consequatur perferendisthat doloribus asperiores repellat.</p>
+            <p>Nam libero tempore cum soluta nobis eligendi optio cumque nihil impedit minusidquod maxime placeat facere possimus.</p>
+            <a  className="more" href="http://zooki.react.themesbrand.com/">LEARN MORE <FaLongArrowAltRight/></a>
+          </div>
+          <div className="image">
+            <img src="http://zooki.react.themesbrand.com/static/media/img-3.1dbfd73268f019064e9f.png" alt="feature-first"/>
           </div>
         </div>
       </div>
