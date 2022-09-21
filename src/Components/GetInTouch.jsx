ac8bc3fe23cf6ec6a358e96e26438618b2c27f65
minahilhussain <minahilh21@gmail.com>
2022-09-21 17:49:16 +0500

    added contact content

--- src/Components/GetInTouch.jsx
+++ src/Components/GetInTouch.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import "../css/GetyInTouch.css"
+import { MdFacebook } from "react-icons/md"
+import { FaTwitter, FaGooglePlusG, FaLinkedin, FaWhatsapp } from "react-icons/fa"
+
+// import { MdLightbulbOutline, MdOutlineNature } from 'react-icons/md';
 
 function GetInTouch() {
   return (
@@ -12,13 +16,25 @@ function GetInTouch() {
         <div className="container">
           <div className="left">
             <input type="text" placeholder="Name"></input>
-            <input type="text" placeholder="Email"></input>
-            <input type="text" placeholder="Subject"></input>
-            <input type="textarea" placeholder="Message"></input>
+            <input type="text" placeholder="Email"></input><br/>
+            <input type="text" placeholder="Subject"></input><br/>
+            <input type="textarea" placeholder="Message"></input><br/>
             <button type="submit" value="Send Message"></button>
-
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <p>Call :</p>
+            <p>012-345-6789</p>
+            <p>Email :</p>
+            <p>youremailid@gmail.com</p>
+            <p>Location :</p>
+            <p>3179 Raccoon Run Seattle, WA 98109</p>
+            <h3>Follow</h3>
+            <MdFacebook/>
+            <FaTwitter/>
+            <FaGooglePlusG/>
+            <FaLinkedin/>
+            <FaWhatsapp/>
+          </div>
 
         </div>
       </div>
